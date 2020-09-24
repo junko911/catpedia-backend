eb03c1ed787f90784d94309e256be4bd206e9d20
junko911 <junkotahara911@gmail.com>
2020-09-24 13:07:24 -0400

    Create cat/breed model/controller

---
+++ db/schema.rb
@@ -0,0 +1,52 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_09_24_160000) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "breeds", force: :cascade do |t|
+    t.string "name"
+    t.string "temperament"
+    t.string "life_span"
+    t.string "wikipedia_url"
+    t.string "origin"
+    t.boolean "rare"
+    t.boolean "hypoallergenic"
+    t.integer "child_friendly"
+    t.integer "dog_friendly"
+    t.integer "energy_level"
+    t.integer "intelligence"
+    t.integer "shedding_level"
+    t.integer "social_needs"
+    t.integer "stranger_friendly"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "cats", force: :cascade do |t|
+    t.string "image"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "avatar"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
