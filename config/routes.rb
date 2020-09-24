17f19c736c416a636d3ff4bf87c8f3b2db0399ff
junko911 <junkotahara911@gmail.com>
2020-09-24 17:06:25 -0400

    Resolve conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :breeds
   resources :cats
   
   get '/categories', to: 'cats#categories'
-=======
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
@@ -12,6 +10,5 @@ Rails.application.routes.draw do
       get '/profile', to: 'users#profile'
     end
   end
->>>>>>> 18ea163c05d8d3c8d2d0471d1b83a09f2cacd463
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
