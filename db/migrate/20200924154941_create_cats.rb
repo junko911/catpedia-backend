a05cc7616362028facf236d6f33928f3be1b826e
junko911 <junkotahara911@gmail.com>
2020-09-24 17:03:14 -0400

    Remove unneeded table

--- db/migrate/20200924154941_create_cats.rb
+++ db/migrate/20200924154941_create_cats.rb
@@ -2,7 +2,7 @@ class CreateCats < ActiveRecord::Migration[6.0]
   def change
     create_table :cats do |t|
       t.string :image
-      t.string :category
+      t.string :cat_id
 
       t.timestamps
     end
