fe87dde0960063a4d3231491aa85814205c5c27c
junko911 <junkotahara911@gmail.com>
2020-09-25 12:35:47 -0400

    Resolve duplication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  resources :breeds
-  resources :cats
-  
-  get '/categories', to: 'cats#categories'
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
