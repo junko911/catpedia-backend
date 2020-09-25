b511df965a7d89ae6985abdccfb024f348a17ef0
junko911 <junkotahara911@gmail.com>
2020-09-25 12:33:03 -0400

    Merge changes

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,17 @@ ActiveRecord::Schema.define(version: 2020_09_24_154941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "cats", force: :cascade do |t|
+    t.string "image"
+    t.string "cat_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.string "avatar"
-  create_table "cats", force: :cascade do |t|
-    t.string "image"
-    t.string "cat_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
