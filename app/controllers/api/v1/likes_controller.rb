e1ce533fd612c552f988772115cbeac03951dcf3
junko911 <junkotahara911@gmail.com>
2020-10-01 17:24:48 -0400

    Modify destory action to accept api_id

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::LikesController < ApplicationController
     skip_before_action :authorized
 
     def destroy
-        @like = Like.find(params[:id])
+        @like = Like.all.find {|e| e.cat.api_id == params[:id]}
         @like.destroy
         render json: @like
     end
