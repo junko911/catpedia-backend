e4666f4f24d39c65f3b66b52160656c9a8bcd73d
junko911 <junkotahara911@gmail.com>
2020-10-01 12:18:18 -0400

    Customize json method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,32 @@ class User < ApplicationRecord
     has_many :followeds, through: :active_relationships, source: :followed
     has_many :passive_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
     has_many :followers, through: :passive_relationships, source: :follower
+
+    def as_json(_options = nil)
+        {
+            id: id,
+            name: name,
+            username: username,
+            avatar: avatar,
+            bio: bio,
+            followers: followers.map { |following|
+                {
+                    id: id,
+                    name: name,
+                    username: username,
+                    avatar: avatar,
+                    bio: bio
+                }
+            },
+            followeds: followeds.map { |followed|
+                {
+                    id: id,
+                    name: name,
+                    username: username,
+                    avatar: avatar,
+                    bio: bio
+                }
+            }
+        }
+    end
 end
