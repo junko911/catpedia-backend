b04610567460338402c09030f833256ad7595fcd
junko911 <junkotahara911@gmail.com>
2020-10-01 00:44:53 -0400

    FIx error

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,9 @@
 class LikesController < ApplicationController
     skip_before_action :authorized
 
-
     def destroy
         @like = Like.find(params[:id])
         @like.destroy
+        render json: @like
     end
 end
