f8a7159e134ce22fb83d06df4cd6f9adb19d9d48
junko911 <junkotahara911@gmail.com>
2020-10-02 09:13:39 -0400

    Change condition to find exact match

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::LikesController < ApplicationController
     # skip_before_action :authorized
 
     def destroy
-        like = Like.all.find {|e| e.cat.api_id == params[:id] || e.user.id === current_user.id}
+        like = Like.all.find {|e| e.cat.api_id == params[:id] && e.user.id === current_user.id}
         like.destroy
         render json: {}
     end
