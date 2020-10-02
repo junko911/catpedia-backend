913703b2400a3fe4137536a99db4f1e23ec0462e
junko911 <junkotahara911@gmail.com>
2020-10-01 23:04:54 -0400

    Remove unneeded lines

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -4,8 +4,6 @@ class Api::V1::LikesController < ApplicationController
     def destroy
         like = Like.all.find {|e| e.cat.api_id == params[:id]}
         like.destroy
-        cat = Cat.all.find {|e| e.api_id == params[:id]}
-        cat.destroy
         render json: {}
     end
 end
